1713ce9a2cf48372dc4dd3c69405cc2de4acf1a1
Ihor Kustov <zegorovskiy1355@gmail.com>
2023-09-29 13:53:43 +0300

    Create signup and login forms

---
+++ core/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
