314cf1e1a4c81863ad35b537d437e816425fc1c2
Ihor Kustov <zegorovskiy1355@gmail.com>
2023-10-02 18:40:40 +0300

    Added new brand filter and update categories filter. Also added search filter. Fixed some bugs.

--- core/views.py
+++ core/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.db.models import Q
 
 from .forms import SignUpForm
 
 from item.models import Item, Brand, Category
 
 def frontpage(request):
-    items = Item.objects.filter(availability=True)[0:10]
-    categories = Category.objects.all()
+    items = Item.objects.filter(availability=True)
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    brand_id = request.GET.get('brand', 0)
     brands = Brand.objects.all()
+
+    if category_id:
+        items = Item.objects.filter(category_id=category_id)
+        brands = set(item.brand_id for item in items)
+
+    if brand_id:
+        items = items.filter(brand_id=brand_id)
+
+    if query:
+        items = items.filter(Q(model__icontains=query) | Q(description__icontains=query))
+
+    categories = Category.objects.all()
 
     return render(request, 'core/frontpage.html', {'items': items,
                                                    'categories': categories,
-                                                   'brands': brands})
+                                                   'brands': brands,
+                                                   'category_id':int(category_id),
+                                                   })
 
 def signup(request):
+    is_login_or_signup_page = request.path 
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
